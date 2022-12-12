70b08d2f63143186082c54c5049882c585cc79cf
Jesús Sánchez Cuadrado <jesusc@um.es>
2022-12-12 18:10:33 +0100

    Example rule to show how to fail

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -6,5 +6,12 @@ echo "  - files: [$INPUT_FILES]"
 
 if [ "$INPUT_FORMAT" = "conga" ]; then
 #  echo `ls /`
-  java -jar /asymob_runSample.jar $INPUT_FILES
+    java -jar /asymob_runSample.jar $INPUT_FILES | tee /tmp/output.txt
+    COUNT=`grep "INP = 0" /tmp/output.txt | wc --lines`
+    if [ $COUNT -gt 2 ]; then
+	echo "More than two intents with INP > 2"
+	exit 1
+    fi
 fi
+
+exit 0
