bc46cd7d1bb206bd1d50ef26ed7e0a60503a5adb
Jesús Sánchez Cuadrado <jesusc@um.es>
2022-12-12 17:35:27 +0100

    Launch Asym0b to compute metrics

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh 
 
-echo "Hello Hola Hola [$0] - [$1] - [$INPUT_FILES]"
+echo "Running action with:"
+echo "  - format: [$INPUT_FORMAT]"
+echo "  - files: [$INPUT_FILES]"
+
+if [ "$INPUT_FORMAT" = "conga" ]; then
+#  echo `ls /`
+  java -jar /asymob_runSample.jar $INPUT_FILES
+fi
